924355ce3e6791b0fa12ca50278ce8bf05ee27bd
KhanAnna <annahan39@gmail.com>
2022-07-15 14:22:58 +0300

    Added urls for main pages

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 
+from main import views
+
 app_name = "main"
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('news/', views.news, name='news'),
+    path('about_university/', views.about_university, name='about_university'),
+    path('chats_university/', views.chats_university, name='chats_university'),
+
+]
