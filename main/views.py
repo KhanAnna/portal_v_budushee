137d363f90d0ca6aae3b40d5427282d3f5b99a0a
KhanAnna <annahan39@gmail.com>
2022-07-15 14:23:14 +0300

    Added views for main pages

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def base(request):
+    return render(request, "main/base.html", {})
+
+
+def home(request):
+    return render(request, "main/home.html", {})
+
+
+def news(request):
+    return render(request, "main/news.html", {})
+
+
+def about_university(request):
+    return render(request, "main/about_university.html", {})
+
+
+def chats_university(request):
+    return render(request, "main/chats_university.html", {})
